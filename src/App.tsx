6e433dc77a81ed487722067aa299e559ac09453b
Leonardo Lopes <l.lopes355@gmail.com>
2022-08-27 17:28:16 -0300

    Feito lado esquerdo do jogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
+import * as C from './App.styles';
+import logoImg from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+import { Button } from './components/Button';
+import { InfoItem } from './components/InfoItem';
+
 const App = () => {
+
+  const resetAndCreateGrid = () => {
+
+  }
+
   return (
-    <div>...</div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImg} width="200" alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          <InfoItem label="Tempo" value="00:00" />
+          <InfoItem label="Movimentos" value="0" />
+        </C.InfoArea>
+
+        <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+      </C.Info>
+      <C.GridArea>
+        ...
+      </C.GridArea>
+    </C.Container>
   );
 }
 
